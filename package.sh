c76a6a216cfa17e0f50394dc17938fd7f37a4ad2
Brad Smith <bradster@infinitewarp.com>
2014-12-01 20:16:59 -0500

    fix file permissions before packaging

--- package.sh
+++ package.sh
@@ -2,5 +2,6 @@
 
 VERSION=$(cat pack.mcmeta | grep -E -o 'v[0-9\.a-z]+' | sed -e 's/v//')
 find assets/ -name '.DS_Store' -exec rm -fv {} \;
+find assets/ -type f -exec chmod -v 644 {} \;
 zip -r AppleNovaBradTextures."$VERSION".zip assets pack.mcmeta pack.png
 mv AppleNovaBradTextures."$VERSION".zip ../
