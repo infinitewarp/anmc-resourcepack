e5bcb42d8dd56b5c0656fc6acf4fdeed7623d229
Brad Smith <bradster@infinitewarp.com>
2019-06-16 22:03:05 -0400

    update package.sh with data directory

--- package.sh
+++ package.sh
@@ -4,11 +4,12 @@ set -e
 git clean -xi
 echo "Cleaning up file modes"
 find assets/ -type f -exec chmod -v 644 {} \;
+find data/ -type f -exec chmod -v 644 {} ;
 
 VERSION=$(jq .pack.description pack.mcmeta | sed -E 's/(.*v([0-9\.a-z]+).*)/\2/')
 FILENAME=AppleNovaBradTextures."${VERSION}".zip
 
 echo "Archiving files"
-zip -r "${FILENAME}" assets pack.mcmeta pack.png
+zip -r "${FILENAME}" assets data pack.mcmeta pack.png
 mv "${FILENAME}" ../
 echo "${FILENAME} created and moved to parent directory"
